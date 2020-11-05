7ce878537cfbf599f03125da3687ffbb709754bc
jonanderson0 <71406833+jonanderson0@users.noreply.github.com>
2020-11-05 12:06:49 -0800

    Update task1.py

--- task1.py
+++ task1.py
@@ -1,1 +1,62 @@
+import tkinter as tk
+from tkinter import *
+import math
 
+win=tk.Tk()
+win.geometry('500x450')
+
+l1=tk.Label(win,text='Enter data about the triangle above\n  I will calculate the area')
+l2=tk.Label(win,text='The area of the triangle is: ')
+
+result=StringVar()
+
+l3=tk.Label(win,textvariable=result)
+
+
+
+def cal():
+    a=(topLeft.get())
+    b=(middleRight.get())
+    c=(bottom.get())
+    h=(height.get())
+    s=0
+    if a!='' and b!=''and c!='':
+        a=float(a)
+        b=float(b)
+        c=float(c)
+        s=(a+b+c)/2
+
+
+        answer=math.sqrt(s*(s-a)*(s-b)*(s-c))
+        answer=round(answer,3)
+
+        result.set(answer)
+
+    elif (a==''or b=='')and c!=''and h!='':
+        c=float(c)
+        h=float(h)
+        answer=(c*h)/2
+        result.set(answer)
+    else :
+        answer='Try again.'
+        result.set(answer)
+
+calc=tk.Button(win,text='Calculate',command=cal)
+
+pic=PhotoImage(file='triangle.png')
+picLabel=tk.Label(win,image=pic)
+topLeft=tk.Entry(win,width=5,borderwidth=3)
+middleRight=tk.Entry(win,width=5,borderwidth=3)
+bottom=tk.Entry(win,width=5,borderwidth=3)
+height=tk.Entry(win,width=5,borderwidth=3)
+
+picLabel.place(x=0,y=0)
+l1.place(x=0,y=275)
+height.place(x=325,y=135)
+topLeft.place(x=160,y=120)
+middleRight.place(x=420,y=180)
+bottom.place(x=225,y=225)
+calc.place(x=275,y=275)
+l2.place(x=0,y=350)
+l3.place(x=160,y=350)
+win.mainloop()
